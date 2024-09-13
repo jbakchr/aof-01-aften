cb095e8fa28b81368d394f0a8a462613cc93eda5
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-09-13 14:02:48 +0200

    Added stuff

---
+++ 02-exercises/03-google-error/main.py
@@ -0,0 +1,4 @@
+"""
+Kør dette program, kopier fejlmeddelelsen fra konsolen og google den for at finde svaret.
+"""
+print("hello world)
---
+++ 02-exercises/04-order-of-execution/main.py
@@ -0,0 +1,6 @@
+"""
+I nedenstående ser du 2 print statements. Hvad sker der, hvis bytter om på rækkefølgen af dem?
+"""
+
+print("Hi")
+print("mom")
